9f46ab70e8f3770098606b62ec84dfa699daa91a
Adrian Petcu <adipetcu@yahoo.com>
2018-06-19 21:22:03 +0300

    Added parcels locations list

--- routes/parcels_locations.js
+++ routes/parcels_locations.js
@@ -9,13 +9,14 @@ const currentTable = 'parcel_locations';
 Restify(parcels_locations, currentTable);
 
 /**
- * Get parcels_locations list
+ * Get parcels list
  */
 parcels_locations.get('/list', function (req, res) {
-    DBInstance.getAll(currentTable).then((data) => {
+    DBInstance.select(currentTable, {parcel: req.query.parcel}).then((data) => {
         res.send(data.map(entry => ParcelLocation(entry)));
     });
 });
+
 
 
 module.exports = parcels_locations;
