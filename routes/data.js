8851a3b6cc65859fec3b7f85d4edeb72d73d6c8e
Adrian Petcu <adipetcu@yahoo.com>
2018-06-20 22:08:42 +0300

    Add counters endpoint

--- routes/data.js
+++ routes/data.js
@@ -11,6 +11,15 @@ data.delete('/delete/:item/:rowId', function (req, res) {
         res.status(400);
         res.send(error);
     })
+
+});
+data.get('/counters', function (req, res) {
+    DBInstance.query('select status,count(id) as nr from parcels group by status').then((data) => {
+        res.send(data);
+    }, (error) => {
+        res.status(400);
+        res.send(error);
+    })
 });
 
 
